104999d1bcffe2907259282dfe9a124002e2c6b9
Alexandra Tinjacá <71520459+Epifis@users.noreply.github.com>
2023-08-23 16:41:35 -0500

    Update ruta.java

--- ruta.java
+++ ruta.java
@@ -8,14 +8,22 @@ public class ruta {
 
     private int id;
     private String origen, destino;
+    private Vuelo vuelo01;
+    private Vuelo vuelo02;
     
     public ruta(){
+        vuelo01 = new Vuelo();
+        vuelo02 = new Vuelo();
         }
     public ruta(String unorigen, String undestino, int unId){
-        this.destino = undestino;
+        this.origen = unorigen;
         this.id = unId;
         this.destino = undestino;
         
+    }
+    public ruta(Vuelo vuelo01, Vuelo vuelo02){
+        this.vuelo01 = vuelo01;
+        this.vuelo02 = vuelo02;
     }
     public int getId (){
         return id;
@@ -35,5 +43,30 @@ public class ruta {
     public void setDestino(String destino){
         this.destino = destino;
     }
+    
+    
+    public void crearVuelo(int identificador, String origen, String destino) {
+
+    }
+    public Vuelo getVuelo01() {
+        return vuelo01;
+    }
+
+    public void setVuelo01(Vuelo vuelo01) {
+        this.vuelo01 = vuelo01;
+    }
+    public Vuelo getVuelo02() {
+        return vuelo02;
+    }
+
+    public void setVuelo02(Vuelo vuelo02) {
+        this.vuelo02 = vuelo02;
+    }
+
+    @Override
+    public String toString() {
+        return "\n      id=" + id + ", origen=" + origen + ", destino=" + destino + "\n          vuelo01=" + vuelo01.toString() + ", vuelo02=" + vuelo02.toString();
+    }
+    
 }
 
