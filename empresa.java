d575d81d7dda0907ff94bc768fc540e18d923749
Alexandra Tinjacá <71520459+Epifis@users.noreply.github.com>
2023-08-22 21:56:07 -0500

    Update empresa.java

--- empresa.java
+++ empresa.java
@@ -42,7 +42,42 @@ public class empresa {
             ;
     }
 
-    public String setRuta(ruta ruta01) {
-        return "empresa{" + "nombre=" + nombre + ", direccion=" + direccion + '}';
+    public void crearRuta(int identificador, String origen, String destino) {
+
     }
+    
+    //Get y set para las rutas
+
+    public Ruta getRuta01() {
+        return ruta01;
+    }
+
+    public void setRuta01(Ruta ruta01) {
+        this.ruta01 = ruta01;
+    }
+
+    public Ruta getRuta02() {
+        return ruta02;
+    }
+
+    public void setRuta02(Ruta ruta02) {
+        this.ruta02 = ruta02;
+    }
+
+    public Ruta getRuta03() {
+        return ruta03;
+    }
+
+    public void setRuta03(Ruta ruta03) {
+        this.ruta03 = ruta03;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa{" + "nombre=" + nombre + ", direccion=" + direccion + ",\n ruta01=" + ruta01.toString() + ",\n ruta02=" + ruta02.toString() + ",\n ruta03=" + ruta03.toString() + '}';
+    }
+    
+    
+
 }
+
